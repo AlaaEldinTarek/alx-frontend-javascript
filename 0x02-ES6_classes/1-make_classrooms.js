118c888e7af289a999a5208939575097a0cfd47b
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2024-01-03 22:48:08 +0300

    done

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,6 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const classroomList = [];
-  const cl1 = new ClassRoom(19);
-  const cl2 = new ClassRoom(20);
-  const cl3 = new ClassRoom(34);
-  classroomList.push(cl1, cl2, cl3);
-  return classroomList;
+  const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return rooms;
 }
