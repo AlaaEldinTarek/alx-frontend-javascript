31f2093727f0ad143668d1d56819ec97632e1ce5
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2024-01-10 23:13:51 +0300

    done

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,6 @@
+export default function updateUniqueItems(map) {
+    if (!(map instanceof Map)) throw Error('Cannot process');
+    for (const element of map) if (element[1] === 1) map.set(element[0], 100);
+    return map;
+  }
+  
